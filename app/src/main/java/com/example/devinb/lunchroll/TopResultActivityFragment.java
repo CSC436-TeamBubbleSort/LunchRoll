a99eca73494029e8d7d32411d77db0ca9658feeb
driv4r <jonwright4944@gmail.com>
2015-10-06 15:51:38 -0700

    Added setup activity skeleton.

---
+++ app/src/main/java/com/example/devinb/lunchroll/TopResultActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.devinb.lunchroll;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TopResultActivityFragment extends Fragment {
+
+    public TopResultActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_top_result, container, false);
+    }
+}
