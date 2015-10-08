b9c2c7a5cf677fbc5ad5c8c8b89b0bbee8f61c90
dmb1 <dmb1@email.arizona.edu>
2015-10-08 00:24:21 -0700

    working on friends list

--- app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/FriendsList.java
+++ app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/FriendsList.java
@@ -4,13 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FriendsList extends AppCompatActivity {
+
+    private String[] friends = {"bob", "susan"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
+        ListView friendsView = (ListView) findViewById(R.id.listView);
+        ArrayList<String> friendsList = new ArrayList<String>();
+        friendsList.addAll(Arrays.asList(friends));
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, R.layout.activity_friends_list, friendsList);
+        friendsView.setAdapter(listAdapter);
     }
 
     @Override
