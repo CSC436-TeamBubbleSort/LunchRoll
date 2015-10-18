7291db4aec57dcd43fe6581b433db32e7532df80
DevinBirtciel <dmb1@email.arizona.edu>
2015-10-18 09:28:49 -0700

    trying to go from login to friends list now 2

--- app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/LoginActivity.java
+++ app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/LoginActivity.java
@@ -189,8 +189,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
         }
-        
-        Intent intent = new Intent(this, FragmentActivity.class);
+
+        Intent intent = new Intent(this, FriendsListActivity.class);
         startActivity(intent);
     }
 
