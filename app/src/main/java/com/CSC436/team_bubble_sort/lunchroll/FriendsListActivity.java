e3a7e09908c44e8cbc966974aefbea8f4a5b930a
DevinBirtciel <dmb1@email.arizona.edu>
2015-10-17 18:05:46 -0700

    tired of messages

--- app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/FriendsListActivity.java
+++ app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/FriendsListActivity.java
@@ -15,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -81,6 +82,8 @@ public class FriendsListActivity extends AppCompatActivity implements OnClickLis
         createGroupPopup.setWidth(popupWidth);
         createGroupPopup.setHeight(popupHeight);
         createGroupPopup.setFocusable(true);
+        createGroupPopup.setWindowLayoutMode(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
 
         // Display the popup
         createGroupPopup.showAtLocation(layout,Gravity.NO_GRAVITY, 0, 0);
