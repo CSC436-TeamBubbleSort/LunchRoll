3619b16ac07c0b41c3dd6c3c692582cd355c743f
DevinBirtciel <dmb1@email.arizona.edu>
2015-10-17 19:41:53 -0700

    tired of messages 8

--- app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/GroupCreateDialog.java
+++ app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/GroupCreateDialog.java
@@ -5,14 +5,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by DevinB on 10/17/2015.
  */
-public class GroupCreateDialog extends DialogFragment {
+public class GroupCreateDialog extends DialogFragment implements  View.OnClickListener{
+    Button createGroup, cancel;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_create_group, null);
+        createGroup = (Button) view.findViewById(R.id.create_group_popup_create_button);
+        cancel = (Button) view.findViewById(R.id.create_group_popup_cancel_button);
+        createGroup.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+        setCancelable(false);
         // Don't need ViewGroup parent so pass null
-        return inflater.inflate(R.layout.fragment_create_group, null);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.create_group_popup_cancel_button){
+            // Add group!
+        }
+        else if(v.getId() == R.id.create_group_popup_create_button){
+            dismiss();
+        }
     }
 }
