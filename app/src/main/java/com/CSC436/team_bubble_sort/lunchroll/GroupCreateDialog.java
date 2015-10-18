558517959bd88590683154993e305252f444e6c9
DevinBirtciel <dmb1@email.arizona.edu>
2015-10-17 20:32:46 -0700

    Trying to get edittext to work 1

--- app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/GroupCreateDialog.java
+++ app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/GroupCreateDialog.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by DevinB on 10/17/2015.
  */
 public class GroupCreateDialog extends DialogFragment implements  View.OnClickListener{
     Button createGroup, cancel;
+    EditText nameBox;
     CommunicateBackToFriendsList communicator;
 
     @Override
@@ -26,6 +29,9 @@ public class GroupCreateDialog extends DialogFragment implements  View.OnClickLi
         View view = inflater.inflate(R.layout.fragment_create_group, null);
         createGroup = (Button) view.findViewById(R.id.create_group_popup_create_button);
         cancel = (Button) view.findViewById(R.id.create_group_popup_cancel_button);
+        nameBox = (EditText) view.findViewById(R.id.create_group_popup_name_field);
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        nameBox.requestFocus();
         createGroup.setOnClickListener(this);
         cancel.setOnClickListener(this);
         setCancelable(false);
@@ -39,7 +45,8 @@ public class GroupCreateDialog extends DialogFragment implements  View.OnClickLi
             dismiss();
         }
         else if(v.getId() == R.id.create_group_popup_create_button){
-            communicator.sendCreateGroupMessageBack("Group Name");
+            String groupName = nameBox.getText().toString();
+            communicator.sendCreateGroupMessageBack(groupName);
             dismiss();
         }
     }
