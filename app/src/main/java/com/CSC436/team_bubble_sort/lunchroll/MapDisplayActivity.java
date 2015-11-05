0fab4352603110cd58e7add3fe722f1cd9f8bf71
Patrick <wilkeningp@gmail.com>
2015-11-04 22:14:47 -0700

    Added class for Map Display? maybe?

---
+++ app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/MapDisplayActivity.java
@@ -0,0 +1,38 @@
+package com.csc436.team_bubble_sort.lunchroll;
+
+/*
+ * Created by PATRICK on 11/4/2015.
+ */
+
+/*
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+*/
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MapDisplayActivity extends Activity /*implements OnMapReadyCallback*/ {
+
+    /*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady( GoogleMap map) {
+        LatLng resturant = new LatLng(0,0);
+
+        map.setMyLocationEnabled(true);
+
+    }
+    */
+
+}
