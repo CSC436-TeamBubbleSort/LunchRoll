19915519f8820060cff06b93418d0b1be9b7894e
dmb1 <dmb1@email.arizona.edu>
2015-12-02 20:35:04 -0700

    fleshed out restaurant entity, and added some final result activity xml, and made minor changes to final result activity java file

--- app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/TopResultActivity.java
+++ app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/TopResultActivity.java
@@ -1,6 +1,5 @@
 package com.csc436.team_bubble_sort.lunchroll;
 
-import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,21 +7,17 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.csc436.team_bubble_sort.lunchroll.entities.Restaurant;
 import com.csc436.team_bubble_sort.lunchroll.web_services.LocationService;
-import com.csc436.team_bubble_sort.lunchroll.web_services.UserService;
 import com.csc436.team_bubble_sort.lunchroll.web_services.location.NearbyAny;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationServices;
 
-public class TopResultActivity extends AppCompatActivity implements NearbyAny{ //ConnectionCallbacks, OnConnectionFailedListener{
+import java.util.List;
+
+public class TopResultActivity extends AppCompatActivity{
 
     private TextView result;
     private LocationService LocationService;
-    //private GoogleApiClient mGoogleApiClient;
-   // Location location;
+    private List<Restaurant> restaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +25,28 @@ public class TopResultActivity extends AppCompatActivity implements NearbyAny{ /
         setContentView(R.layout.activity_top_result);
         result = (TextView) findViewById(R.id.result);
         LocationService = new LocationService(this.getApplicationContext());
-       /* mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();*/
-        nearbyAnyRequest();
+        //nearbyAnyRequest();
+        defaultRestaurants();
     }
 
-   /* @Override
-    public void onConnected(Bundle bundle) {
-       // Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
-    }*/
+    private long latitude;
+    private long longitude;
+    private String iconURL;
+    private String name;
+    private String priceLevel;
+    private String rating;
+    private String crossRoads; //Vicinity in the object
+    private boolean openNow;
+    //TODO delete this hardcoded list when we start making server calls
+    private void defaultRestaurants(){
+        Restaurant rest1 = new Restaurant(12,15,"notAURL","Bob's Burgers", "expensivo",
+                "not very good", "near and far", true);
+        Restaurant rest2 = new Restaurant(13,16,"notEvenAlmostAURL","Little Italy pasta", "cheap",
+                "excellent", "close and closer", false);
+        restaurants.add(rest1);
+        restaurants.add(rest2);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -57,8 +61,6 @@ public class TopResultActivity extends AppCompatActivity implements NearbyAny{ /
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -66,47 +68,18 @@ public class TopResultActivity extends AppCompatActivity implements NearbyAny{ /
         return super.onOptionsItemSelected(item);
     }
 
-    public void nearbyAnyRequest(){
-        //this.LocationService.nearbyAny(this, location.getLatitude(), location.getLongitude());
-        this.LocationService.nearbyAny(this, 32, -111);
-    }
-    public void nearbyAnySuccess(String result){
-        this.result.setText(result);
-    }
-    public void nearbyAnyError(String error){
-        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
-    }
-
-   /* @Override
-    public void onConnectionSuspended(int cause) {
-        // The connection has been interrupted.
-        // Disable any UI components that depend on Google APIs
-        // until onConnected() is called.
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        // This callback is important for handling errors that
-        // may occur while attempting to connect with Google.
-        //
-        // More about this in the 'Handle Connection Failures' sections
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        if (mGoogleApiClient!=null) {
-            mGoogleApiClient.connect();
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        if (mGoogleApiClient!=null) {
-            mGoogleApiClient.disconnect();
-        }
-
-        super.onStop();
-    }*/
+//    // Called by Activity to send request to server with user data
+//    public void nearbyAnyRequest(int latitude, int longitude){
+//        this.LocationService.nearbyAny(this, latitude, longitude);
+//    }
+//    // Web service call ends up calling this when it gets a response back from server
+//    // with requested restaurant data
+//    public void nearbyAnySuccess(List<Restaurant> results){
+//        // TODO make GUI reflect this list of restaurants
+//    }
+//    // Called if the server was unable to get requested data
+//    public void nearbyAnyError(String error){
+//        // TODO maybe handle error
+//        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+//    }
 }
--- app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/entities/Restaurant.java
+++ app/src/main/java/com/CSC436/team_bubble_sort/lunchroll/entities/Restaurant.java
@@ -1,42 +1,117 @@
 package com.csc436.team_bubble_sort.lunchroll.entities;
 
+import android.util.Log;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
  * Created by Jonathan on 11/15/2015.
  */
 public class Restaurant {
-    private long latitutde;
+    private long latitude;
     private long longitude;
     private String iconURL;
     private String name;
     private String priceLevel;
     private String rating;
-    private String crossRoads; //Vicitinty in the object
+    private String crossRoads; //Vicinity in the object
     private boolean openNow;
 
-
     public Restaurant(JSONObject result){
-
+        if(result == null){
+            Log.e("restaurant", "Trying to parse null result");
+        }
+        try {
+            JSONObject location = result.getJSONObject("geometry").getJSONObject("location");
+            latitude = location.getLong("lat");
+            longitude = location.getLong("lng");
+            iconURL = result.optString("icon");
+            name = result.getString("name");
+            priceLevel = result.optString("price_level");
+            rating = result.optString("rating");
+            crossRoads = result.optString("vicinity");
+            openNow = result.optJSONObject("opening_hours").getBoolean("open_now");
+        } catch (JSONException e) {
+            Log.e("restaurant", "Error parsing JSON in Restaurant constructor");
+            e.printStackTrace();
+        }
     }
 
-    public void setLatitude(){
-
+    // Used for making hard coded restaurants
+    public Restaurant(long latitude, long longitude, String iconURL, String name,
+                      String priceLevel, String rating, String crossRoads, boolean openNow){
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.iconURL = iconURL;
+        this.name = name;
+        this.priceLevel = priceLevel;
+        this.rating = rating;
+        this.crossRoads = crossRoads;
+        this.openNow = openNow;
     }
 
-    public void getLatitude(){
-
+    public void setLatitude(long latitude){
+        this.latitude = latitude;
     }
 
-    public void setLongitude(){
-
+    public long getLatitude(){
+        return latitude;
     }
 
-    public void getLongitude(){
-
+    public void setLongitude(long longitude){
+        this.longitude = longitude;
     }
 
+    public long getLongitude(){
+        return longitude;
+    }
 
+    public String getPriceLevel() {
+        return priceLevel;
+    }
 
+    public void setPriceLevel(String priceLevel) {
+        this.priceLevel = priceLevel;
+    }
 
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getCrossRoads() {
+        return crossRoads;
+    }
+
+    public void setCrossRoads(String crossRoads) {
+        this.crossRoads = crossRoads;
+    }
+
+    public boolean isOpenNow() {
+        return openNow;
+    }
+
+    public void setOpenNow(boolean openNow) {
+        this.openNow = openNow;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIconURL() {
+        return iconURL;
+    }
+
+    public void setIconURL(String iconURL) {
+        this.iconURL = iconURL;
+    }
 }
